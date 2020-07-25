106ed2414a04d465c7c6224a1d31c60567f56cb3
Hamish Coleman <hamish@zot.org>
2020-07-25 10:40:37 +0100

    I would prefer to use a tool that doesnt break, but since they hate devs, try and work around them

---
+++ arduino_esp32cam_example.ino
@@ -0,0 +1,19 @@
+#if 0
+/*
+
+Repeat after me:
+- I must break all assumptions about how compiling works
+- I must change my mind on minor version releases
+- I must claim to be a CLI tool, but adhere to strange IDE rules
+
+Oh, wait, none of these are useful.
+
+A file with the same name as the subdirectory must exist since arduino-cli 0.11
+otherwise it thinks there is no sketch here.  Once this file exists, it will
+happily compile /all/ the .ino files in this directory
+
+Grr
+
+
+ */
+#endif
